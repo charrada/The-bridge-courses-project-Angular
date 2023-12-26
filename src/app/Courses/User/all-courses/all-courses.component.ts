1de2420b44bb507be5400331405b73f570dce967
Mohamed Aziz Charrada <mohamedaziz.charrada@esprit.tn>
2023-12-27 00:11:08 +0100

    Affichage courses

--- src/app/Courses/User/all-courses/all-courses.component.ts
+++ src/app/Courses/User/all-courses/all-courses.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { course } from 'src/app/models/course.model';
+import { CoursesService } from '../../courses.service';
 
 @Component({
   selector: 'app-all-courses',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AllCoursesComponent {
 
+  courses: course[] = []; // Assuming course array is named 'courses'
+
+  constructor(private coursesService: CoursesService) { }
+
+  ngOnInit(): void {
+    // Fetch courses from the service and assign them to the 'courses' array
+    this.coursesService.getAllCourses().subscribe(data => {
+      this.courses = data;
+    });
+  }
 }
--- src/app/Courses/courses.service.ts
+++ src/app/Courses/courses.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { course } from '../models/course.model'; // Import your Course model
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
+  private apiUrl = 'http://localhost:8089/Bridge'; // Replace with your API base URL
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  addCourse(course: course): Observable<course> {
+    return this.httpClient.post<course>(`${this.apiUrl}/addCourse`, course);
+  }
+
+  updateCourse(course: course): Observable<course> {
+    return this.httpClient.put<course>(`${this.apiUrl}/updateCourse`, course);
+  }
+
+  deleteCourse(courseId: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}/deleteCourse/${courseId}`);
+  }
+
+  getAllCourses(): Observable<course[]> {
+    return this.httpClient.get<course[]>(`${this.apiUrl}/`);
+  }
 }
